a4c84e5784865e590cdc74df75dddff394799ba9
lerandc <lerandc@umich.edu>
2018-07-03 13:10:08 -0500

    Memory Calculations

--- matlab scripts/fourier_space_testing.m
+++ matlab scripts/fourier_space_testing.m
@@ -7,8 +7,15 @@ E0 = 300e3;
 lambda = 1e10*(h/(sqrt(2*m*e*E0)))/(sqrt(1+(e*E0)/(2*m*c*c)));
 
 real_pixel_size = 0.00975;
+<<<<<<< Updated upstream
 cell_dim = 60;
 f = 2;
+=======
+cell_dim = 50;
+z_size = 347.65;
+slice_thickness = 1.5;
+f = 4;
+>>>>>>> Stashed changes
 f_x = 4*f;
 im_size = f_x*round(cell_dim/(f_x*real_pixel_size));
 
@@ -18,7 +25,36 @@ dpx = 1/(im_size*real_pixel_size);
 ncx = floor(im_size)/2;
 qmax = dpx*ncx/2;
 alpha_max = 1e3*qmax*lambda;
+d_step = 1;
+d_vec = d_step/2 : d_step : alpha_max-d_step/2;
+Ndet = ceil(alpha_max);
+N_streams = 4;
+N_gpu = 4;
+total_streams = N_gpu*N_streams;
+batch_size = 4;
+im_size = im_size*(1/1);
 
+N_planes = floor(z_size/slice_thickness) + (mod(z_size,slice_thickness) ~= 0);
+
+est_memory_device_single = (im_size*im_size)*(8+8*N_planes+8+4+4+4+...
+    N_streams*(batch_size*8+batch_size*4))+(N_streams*(Ndet*4));
+
+est_memory_device_stream = (im_size*im_size)*(8+8+4+4+4+...
+    N_streams*(8+batch_size*8+batch_size*4))+N_streams*((Ndet*4));
+
+est_memory_host = (im_size*im_size)*(8+8*N_planes+8+4+4+4)+...
+    total_streams*(Ndet*4);
+
+actual_host = 40004247168;
+actual_stream = 5350889376;
+actual_single_xfer = 44081092512;
+
+if im_size > 2000
+    manual_override = 0;
+    if ~manual_override
+        error('reset override before you crash your laptop thanks')
+    end
+end
 qxInd = zeros(im_size/2,1);
 
 n_1 = im_size;
